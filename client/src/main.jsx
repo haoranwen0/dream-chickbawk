d4034b41ddee489d66dcdc2818566f8ba4828769
haoranwen0 <hranwen@mit.edu>
2024-05-17 17:57:12 -0400

    initialized

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import './main.css'
+import './styles/App.css'
+
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 )
